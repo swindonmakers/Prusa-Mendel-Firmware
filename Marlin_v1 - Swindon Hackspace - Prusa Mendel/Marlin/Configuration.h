9823a3361b347b3f8bcfd02ef2f70f96b7151ea5
Jamie Osborne <jmeosbn@gmail.com>
2014-12-24 17:42:46 +0000

    Revert retract acceleration to 3000

--- Marlin_v1 - Swindon Hackspace - Prusa Mendel/Marlin/Configuration.h
+++ Marlin_v1 - Swindon Hackspace - Prusa Mendel/Marlin/Configuration.h
@@ -390,7 +390,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          500    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  500   // X, Y, Z and E max acceleration in mm/s^2 for retracts
+#define DEFAULT_RETRACT_ACCELERATION  3000   // X, Y, Z and E max acceleration in mm/s^2 for retracts
 
 // Offset of the extruders (uncomment if using more than one and relying on firmware to position when changing).
 // The offset has to be X=0, Y=0 for the extruder 0 hotend (default extruder).
