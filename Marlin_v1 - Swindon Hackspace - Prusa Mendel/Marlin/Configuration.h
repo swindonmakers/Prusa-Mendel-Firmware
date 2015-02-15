d3315a4d37d097899a3ef54b60c18c3077490414
Jamie Osborne <jmeosbn@gmail.com>
2015-02-15 21:07:52 +0000

    Increase default max feedrate for extruder

--- Marlin_v1 - Swindon Hackspace - Prusa Mendel/Marlin/Configuration.h
+++ Marlin_v1 - Swindon Hackspace - Prusa Mendel/Marlin/Configuration.h
@@ -386,7 +386,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // default settings
 
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {64.2,64.1,2560,95.5} // default steps per unit
-#define DEFAULT_MAX_FEEDRATE          {500, 500, 4,  25}        // (mm/sec)
+#define DEFAULT_MAX_FEEDRATE          {500, 500, 4,  80}        // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,5000}      // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          500    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
